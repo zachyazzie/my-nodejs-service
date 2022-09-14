61334b170c02a04cfaec9700b1cc76298c685afa
Zach <zacharyyazzie94@gmail.com>
2022-09-14 12:02:20 -0600

    Change message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node Server!');
+    res.send('Hal Jordan');
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
