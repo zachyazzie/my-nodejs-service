be7fa2295b4bfc6f015b6d56024cdb176c225056
Zach <zacharyyazzie94@gmail.com>
2022-09-29 19:43:46 -0600

    fix

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-require('dotenv').config({ path: 'ENV_FILENAME' });
+const dotenv = require('dotenv');
+dotenv.config();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json()).use('/', require('./routes'));
 
 //Connect to DB
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
-  }
-});
+  })
+  .catch((err) => {
+    console.error('Error connecting to Mongo', err);
+  });
